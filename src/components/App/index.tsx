d1c2d0d136d5d098e1ed168d9e4963b2f5ecf37f
nenadV91 <nenadwebdevelop@gmail.com>
2023-01-24 15:12:13 +0100

    Initial web3-onboard implementation

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import { WalletDetails } from "../WalletDetails";
+
+function App() {
+  return (
+    <div className="app">
+      <nav>
+        <WalletDetails />
+      </nav>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { Web3OnboardProvider } from "@web3-onboard/react";
+import { web3Onboard } from "./components/WalletDetails";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Web3OnboardProvider web3Onboard={web3Onboard}>
+      <App />
+    </Web3OnboardProvider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
