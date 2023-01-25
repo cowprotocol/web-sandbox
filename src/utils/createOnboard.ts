b86b4ffa73585f321fa555057be8e2772a752dda
nenadV91 <nenadwebdevelop@gmail.com>
2023-01-25 12:45:46 +0100

    Added ledger, keystone and coinbase wallets

--- src/utils/createOnboard.ts
+++ src/utils/createOnboard.ts
@@ -1,6 +1,10 @@
 import { init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
 import walletConnectModule from "@web3-onboard/walletconnect";
+import keystoneModule from "@web3-onboard/keystone";
+import ledgerModule from "@web3-onboard/ledger";
+import coinbaseWalletModule from "@web3-onboard/coinbase";
+
 import { ethereumMainet, polygonMainnet } from "../constants/chains";
 
 export const createOnboard = () => {
@@ -9,6 +13,10 @@ export const createOnboard = () => {
   const chains = [ethereumMainet, polygonMainnet];
 
   const injected = injectedModule();
+  const keystone = keystoneModule();
+  const ledger = ledgerModule();
+  const coinbaseWalletSdk = coinbaseWalletModule();
+
   const walletConnect = walletConnectModule({
     bridge: WC_BRIDGE_URL,
     qrcodeModalOptions: {
@@ -24,12 +32,22 @@ export const createOnboard = () => {
     connectFirstChainId: true,
   });
 
-  const wallets = [injected, walletConnect];
+  const wallets = [
+    injected,
+    walletConnect,
+    keystone,
+    ledger,
+    coinbaseWalletSdk,
+  ];
 
   const appMetadata = {
     name: "CoW Swap",
     icon: "/images/cow-logo.svg",
     description: "Moooooooooooo",
+    recommendedInjectedWallets: [
+      { name: "MetaMask", url: "https://metamask.io" },
+      { name: "Coinbase", url: "https://wallet.coinbase.com/" },
+    ],
   };
 
   const web3Onboard = init({
