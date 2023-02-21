5deeecfe49e09b7d2408fad3baa4da6035553bc7
nenadV91 <nenadwebdevelop@gmail.com>
2023-02-21 14:04:18 +0100

    Separate ConnectOption component

--- src/components/WalletDetails/index.tsx
+++ src/components/WalletDetails/index.tsx
@@ -1,17 +1,14 @@
-import { useAccount, useConnect, useDisconnect } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { LedgerConnector } from "wagmi/connectors/ledger";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { mainnet, gnosis, goerli } from "wagmi/chains";
-import { Connector } from "wagmi";
 import { Buffer } from "buffer";
+import { ConnectOption } from "./ConnectOption";
 import CoinbaseIcon from "assets/icons/coinbase.svg";
 import MetamaskIcon from "assets/icons/metamask.png";
 import WalletConnectIcon from "assets/icons/walletconnect.svg";
-import ZengoIcon from "assets/icons/zengo.svg";
 import LedgerIcon from "assets/icons/ledger.svg";
-import styled from "styled-components";
 
 // polyfill Buffer for client
 if (!window.Buffer) {
@@ -39,55 +36,6 @@ const walletConnect = new WalletConnectConnector({
     qrcode: true,
   },
 });
-
-type ConnectType = {
-  connector: Connector;
-  name: string;
-  icon: string;
-};
-
-const ConnectButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 10px;
-  border: 1px solid blue;
-  border-radius: 5px;
-  background: transparent;
-  width: auto;
-  margin-right: 10px;
-  cursor: pointer;
-
-  img {
-    margin-right: 5px;
-    max-width: 30px;
-  }
-`;
-
-const ConnectOption = ({ connector, name, icon }: ConnectType) => {
-  const { connect } = useConnect({ connector });
-  const { disconnect } = useDisconnect();
-  const { isConnecting, isConnected, connector: c } = useAccount();
-
-  const isThisConnected = isConnected && connector.id === c?.id;
-  const isThisConnecting = isConnecting && connector.id === c?.id;
-
-  return (
-    <ConnectButton
-      disabled={isConnecting}
-      onClick={() => (isThisConnected ? disconnect() : connect())}
-    >
-      <img src={icon} alt={`${name} icon`} />
-      <span>
-        {isThisConnecting
-          ? "connecting"
-          : isThisConnected
-          ? "disconnect"
-          : `${name}`}
-      </span>
-    </ConnectButton>
-  );
-};
 
 export const WalletDetails = () => {
   return (
